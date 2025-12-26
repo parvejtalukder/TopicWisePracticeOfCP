a290a6085b7cd266f742690764c091b0fde7b288
parvejtalukder <mail@parvejhusentalukder.com>
2025-12-27 01:43:59 +0600

    Update: New Changes

--- AdhocMadness/Memset/Memset.cpp
+++ AdhocMadness/Memset/Memset.cpp
@@ -8,6 +8,12 @@ void pht() {
 
 int main() {
     pht();
-    
+    int n, i;
+    cin >> n >> i;
+    if (n & (1 << i)) {
+        cout << "true" << "\n";
+    } else {
+        cout << "false" << "\n";
+    }
     return 0;
 }    
