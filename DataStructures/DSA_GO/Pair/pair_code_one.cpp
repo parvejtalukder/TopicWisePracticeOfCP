a6306f84f6d9c7befc789674c84a8298bb0beb93
parvejtalukder <mail@parvejhusentalukder.com>
2026-01-01 01:42:05 +0600

    Update: New Chnages

---
+++ DataStructures/DSA_GO/Pair/pair_code_one.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pht() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+}
+
+int main() {
+    pht();
+    pair <string, int> ph;
+    // pair <string, int> ph[10];
+    ph = make_pair("Parvej", 48002);
+    auto [n, r] = ph;
+    cout << n << " " << r << "\n";
+    return 0;
+}
