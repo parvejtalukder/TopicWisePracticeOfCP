51b6fdc462c3d32450f5906bf494a4274d1b164d
parvejtalukder <mail@parvejhusentalukder.com>
2026-01-01 15:51:01 +0600

    Update: New chages

---
+++ DataStructures/DSA_GO/stack_code.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pht() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+}
+
+int main() {
+    pht();
+    stack <int> st;
+    
+    return 0;
+}
