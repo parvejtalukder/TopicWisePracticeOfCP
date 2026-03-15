036ad263aeb5220dd329a810b86b48fda69ff65d
parvejtalukder <mail@parvejhusentalukder.com>
2026-03-15 23:52:38 +0600

    Update: New Changes

---
+++ TwoPointer/Prime/DivisorFrom1ToN.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pht() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int divisorCnt(long long n) {
+    int cnt = 0;
+    for(int i = 1; 1LL * i * i <= n; i++) {
+        if (n % i == 0) {
+            ++cnt;
+            if (i != n/i) {
+                ++cnt;
+            }
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    pht();
+    long long n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        cout << divisorCnt(i) << "\n";
+    }
+    // cout << "\n";
+    return 0;
+}
