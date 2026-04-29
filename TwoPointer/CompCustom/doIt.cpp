0ef87d9e5eae75c54820e1c02fc1f27203dab63c
parvejtalukder <mail@parvejhusentalukder.com>
2026-04-29 10:52:09 +0600

    Update: New Changes

---
+++ TwoPointer/CompCustom/doIt.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pht() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {
+    pht();
+    vector <pair<int, int>> arr;
+    arr ={{1,2}, {2,3}, {3,4}, {4, 5}, {5, 6}, {6,7}, {7,8}};
+    
+    return 0;
+}
